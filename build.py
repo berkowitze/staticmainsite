78a0154f9c87f0b1b71a5e6e4411e83f0746db7c
Elias Berkowitz <eliberkowitz@gmail.com>
2019-01-14 15:50:36 -0500

    testing netlify

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
+import json
+
+with open('stuff.json') as f:
+    data = json.load(f)
+
+html = '<html><body>'
+
+for x in data:
+    html += '<div>' + str(x) + '</div>'
+
+html += '</body></html>'
+
 with open('lol.html', 'w') as f:
-    f.write('ugh')
+    f.write(html)
